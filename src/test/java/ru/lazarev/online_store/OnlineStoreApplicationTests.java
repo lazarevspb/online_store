32c57cf4435ecfbe5d808b64e2abfa38e62a38f6
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-04-17 11:12:12 +0300

    init commit

---
+++ src/test/java/ru/lazarev/online_store/OnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lazarev.online_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
