e429e48f8075cee482506c2c04b25f0ea31632f6
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-05-07 11:35:57 +0300

    added Swagger

--- src/main/java/ru/lazarev/online_store/controllers/AuthController.java
+++ src/main/java/ru/lazarev/online_store/controllers/AuthController.java
@@ -1,5 +1,9 @@
 package ru.lazarev.online_store.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,14 +18,25 @@ import ru.lazarev.online_store.dto.JwtResponse;
 import ru.lazarev.online_store.services.UserService;
 
 
+
 @RestController
 @AllArgsConstructor
+
+@Api("Set of endpoints for new user authentication")
 public class AuthController {
     private UserService userService;
     private JwtTokenUtil jwtTokenUtil;
     private AuthenticationManager authenticationManager;
 
     @PostMapping("/auth")
+    @ApiOperation("Creates token for user")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", defaultValue = "bob1", required = true,
+                    dataTypeClass = String.class, type = "String", paramType = "query",
+                    value = "Username"),
+            @ApiImplicitParam(name = "password", defaultValue = "111", required = true,
+                    dataTypeClass = String.class, type = "String", paramType = "query",
+                    value = "Password")})
     public ResponseEntity<?> createToken(@RequestBody JwtRequest jwtRequest) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(),
                 jwtRequest.getPassword()));
--- src/main/java/ru/lazarev/online_store/dto/JwtRequest.java
+++ src/main/java/ru/lazarev/online_store/dto/JwtRequest.java
@@ -1,9 +1,15 @@
 package ru.lazarev.online_store.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel
 public class JwtRequest {
+    @ApiModelProperty(notes = "Username", required = true, position = 1, example = "bob1")
     private String username;
+
+    @ApiModelProperty(notes = "Users password ", required = true, position = 2, example = "111")
     private String password;
 }
